e7bf7d541ff7af03bf4ab093512efbf157fadc85
Brian Babu <briansmkenya@gmail.com>
2023-04-12 21:54:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+function fuzzyMatch(drivers, beginningLetters) {
+    return drivers.filter(function (driver) {
+        return driver.slice(0, 2) === beginningLetters;
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function (driver) {
+        return (driver.name === string)
+    })
+}
